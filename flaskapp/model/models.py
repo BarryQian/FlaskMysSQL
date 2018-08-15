5a3b7131eacec36cdaab90814761be8b8fad9111
qg <1490020955@qq.com>
2018-08-15 10:39:15 +0800

    添加了表单，提交数据到数据库

--- flaskapp/model/models.py
+++ flaskapp/model/models.py
@@ -5,11 +5,21 @@ class Stu(db.Model):
     __tablename__ = 'stu'
     id = db.Column(db.Integer, nullable=False, primary_key=True, autoincrement=True)
     name = db.Column(db.String(16), nullable=False, server_default='', unique=True)
-    age = db.Column(db.Integer, nullable=False)
+    age = db.Column(db.String(16), nullable=False)
 
-    def get_list():
+    def get_list(self):
         users = Stu.query.all()
         return users
+
+    def add_user(self, username, userage):
+        try:
+            db.session.add(Stu(name=username, age=userage))
+            db.session.commit()
+            print('写入数据库测试点：成功')
+            return True
+        except Exception as e:
+            print('写入数据库测试点：失败')
+            return False
 
 
 
--- flaskapp/views.py
+++ flaskapp/views.py
@@ -1,5 +1,5 @@
 from flaskapp.__init__ import app
-from flask import render_template
+from flask import render_template, request
 from flaskapp.model.models import Stu
 
 
@@ -11,9 +11,26 @@ def hello_world():
 @app.route('/get_list')
 def get_list():
 
-    #获取数据
-    users = Stu.get_list()
+    #调用数据库获取数据（表格对象）
+    userObj = Stu()
+    #处理数据，获得表格内容
+    users = userObj.get_list()
     print(users)
 
     return render_template('user.html', users=users)
 
+
+@app.route('/add_user', methods=['POST', 'GET']) #此处的POST GET 必须大写
+def add_user():
+    if request.method == 'POST':
+        username = request.form['username']
+        userage = request.form['userage']
+        print("用户名测试点:" + username + userage)
+        #调用数据库数据
+        userObj = Stu()
+        res = userObj.add_user(username, userage)
+
+        if res:
+            return render_template('ok.html')
+        else:
+            return render_template('error.html')
